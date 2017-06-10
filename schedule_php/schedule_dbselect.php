c58af53816ea483eb2d1ce9b9bf7456600595d33
finite0104 <how3265@naver.com>
2017-06-11 00:00:36 +0900

    mysqli query fix

--- schedule_php/schedule_dbselect.php
+++ schedule_php/schedule_dbselect.php
@@ -1,12 +1,12 @@
 <?php
     $current_time = date("Y-m-j H:i:s");
-    $connect = mysqli_connect("localhost:3306", "smartmirror", "root", "flvmfptl1");
-    mysqli_set_charset($connect, "utf8");
+    $connect = mysql_connect("localhost:3306", "smartmirror", "root", "flvmfptl1");
+    mysql_set_charset($connect, "utf8");
 
     $selectSQL = "select * from schedule where schedule_time >= '$current_time'";
-    $selectResult = mysqli_query($connect, $selectSQL);
+    $selectResult = mysql_query($connect, $selectSQL);
 
-    $selectRowCount = mysqli_num_rows($selectResult);
+    $selectRowCount = mysql_num_rows($selectResult);
 
     echo "rowCount : ";
     echo $selectRowCount;
@@ -15,11 +15,11 @@
         echo "검색 결과가 없음";
     }else if($selectRowCount >= 1) {
         //검색된 행 존재 -> 결과 표시
-        while($row = mysqli_fetch_row($selectResult)) {
+        while($row = mysql_fetch_row($selectResult)) {
             echo "Schedule_Time : $row[1] ";
             echo "Subject : $row[2] ";
             echo "<br />";
         }
     }
-    mysqli_close($connect);
+    mysql_close($connect);
 ?>
