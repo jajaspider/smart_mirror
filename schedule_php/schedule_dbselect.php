12992ef86dadff2dcee0e481bf56469ee299cc46
finite0104 <how3265@naver.com>
2017-06-10 22:55:16 +0900

    schedule - php

---
+++ schedule_php/schedule_dbselect.php
@@ -0,0 +1,20 @@
+<?php
+    $current_time = date("Y-m-j H:i:s");
+    $connect = mysqli_connect("localhost:3306", "smartmirror", "root", "flvmfptl1");
+    mysqli_set_charset($connect, "utf8");
+
+    $selectSQL = "select * from schedule where schedule_time >= '$current_time'";
+    $selectResult = mysqli_query($connect, $selectSQL);
+
+    if(mysqli_num_rows($selectResult) == 0) {
+        echo "검색 결과가 없음";
+    }else(mysqli_num_rows($selectResult) >= 1) {
+        //검색된 행 존재 -> 결과 표시
+        while($row = mysql_fetch_row($selectResult)) {
+            echo "Schedule_Time : $row[1] ";
+            echo "Subject : $row[2] ";
+            echo "<br />";
+        }
+    }
+    mysqli_close($connect);
+?>
