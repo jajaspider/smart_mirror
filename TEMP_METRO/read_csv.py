1c57316ada69650b50686e0d9a43fa16c0c863c6
finite0104 <how3265@naver.com>
2017-06-13 23:33:22 +0900

    metro db insert'

--- TEMP_METRO/read_csv.py
+++ TEMP_METRO/read_csv.py
@@ -1,7 +1,19 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
 import csv
+import pymysql
+
+conn = pymysql.connect(host='localhost', user='root', password='flvmfptl1', db='smartmirror', charset='utf8')
+cursor = conn.cursor()
+sql = """insert into metro_station values (NULL, %s, %s, %s, %s)"""
 
 f = open('metro.csv', 'r')
 rdr = csv.reader(f)
+#rdr = codecs.open('metro.csv','r',encoding='utf-8')
 
 for line in rdr:
+    cursor.execute(sql, (line[1], line[3], line[2], line[4]))
+    conn.commit()
     print(line[1] + "역 " + " Y : " + line[2] + " X: " + line[3] +" 역코드 : "+line[4])
+
+conn.close()
